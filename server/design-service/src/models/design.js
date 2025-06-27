16939d74af3aeee8ec44c8396bd1ef54de846545
saurabh-develop <saurabhkhare893@gmail.com>
2025-06-27 23:58:14 +0530

    design-service and schema are made

---
+++ server/design-service/src/models/design.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const DesignSchema = new mongoose.Schema({
+  userId: String,
+  name: String,
+  canvasData: String,
+  width: Number,
+  height: Number,
+  category: String,
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const Design = mongoose.models.Design || mongoose.model("Design", DesignSchema);
+module.exports = Design;
--- server/design-service/src/server.js
+++ server/design-service/src/server.js
@@ -3,7 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import helmet from "helmet";
-// import designRoutes from "./routes/design-routes";
+import designRoutes from "./routes/design-routes";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
@@ -18,7 +18,7 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use("/api/designs", designRoutes);
+app.use("/api/designs", designRoutes);
 
 async function startServer() {
   try {
